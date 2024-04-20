922c419e87a45ae8b6ecc3f37dd385c37a905203
Nasim <nasimakhtarkhan9@gmail.com>
2024-04-20 23:09:57 +0530

    some error fixed

--- src/componenets/WorkCardData.js
+++ src/componenets/WorkCardData.js
@@ -1,5 +1,4 @@
 import pro1 from "../assets/to-do-list.png"
-import pro2 from "../assets/colorgradient.webp"
 import pro3 from "../assets/news.webp"
 import pro4 from "../assets/shopsy.png"
 import pro5 from "../assets/cal.png"
@@ -18,17 +17,18 @@ const ProjectCardData = [
         text: "A simple calculator for daily calculation  ",
         view:"https://nakcalculator.netlify.app"
       },
+    
+      { imgsrc: pro6,
+        title: "Food-App",
+        text: "Food-App with different categories and search functionality",
+        view:"https://undermodification.netlify.app"
+      },
       {
         imgsrc: pro8,
         title: "Colourgenerator ",
         text: " A website for generating background color of a webpage.",
         view:"https://bgcolourgenerator.netlify.app"
     },
-      { imgsrc: pro6,
-        title: "Food-App",
-        text: "Food-App with different categories and search functionality",
-        view:"https://undermodification.netlify.app"
-      },
   
     {
         imgsrc: pro1,
