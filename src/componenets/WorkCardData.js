4c95e7f5c54df86389225d7b556d2cec7ee86e3d
Nasim <nasimakhtarkhan9@gmail.com>
2024-04-20 23:03:43 +0530

    added coming soon for undermodification

--- src/componenets/WorkCardData.js
+++ src/componenets/WorkCardData.js
@@ -4,7 +4,6 @@ import pro3 from "../assets/news.webp"
 import pro4 from "../assets/shopsy.png"
 import pro5 from "../assets/cal.png"
 import pro6 from "../assets/food.png"
-import pro7 from "../assets/Colorpick.png"
 import pro8 from "../assets/Designer.png"
 
 const ProjectCardData = [
@@ -19,29 +18,30 @@ const ProjectCardData = [
         text: "A simple calculator for daily calculation  ",
         view:"https://nakcalculator.netlify.app"
       },
+      {
+        imgsrc: pro8,
+        title: "Colourgenerator ",
+        text: " A website for generating background color of a webpage.",
+        view:"https://bgcolourgenerator.netlify.app"
+    },
       { imgsrc: pro6,
         title: "Food-App",
         text: "Food-App with different categories and search functionality",
-        view:"https://nakshopsy.netlify.app"
+        view:"https://undermodification.netlify.app"
       },
   
     {
         imgsrc: pro1,
         title: "To-Do-List",
         text: " Your Daily App for your to-do list, you can manage task easily.",
-        view:"https://nakshopsy.netlify.app"
+        view:"https://undermodification.netlify.app"
     },
-    {
-        imgsrc: pro8,
-        title: "Colourgenerator ",
-        text: " A website for generating background color of a webpage.",
-        view:"https://bgcolourgenerator.netlify.app"
-    },
+  
     {
         imgsrc: pro3,
         title: "News Website",
         text: " Get latest and Breaking News from around the world.",
-        view:"https://nakshopsy.netlify.app"
+        view:"https://undermodification.netlify.app"
     },
  
 
