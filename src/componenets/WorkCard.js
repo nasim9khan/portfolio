c6689274093e753f194f8792d4e654d5cc4a9608
Nasim <nasimakhtarkhan9@gmail.com>
2024-07-05 21:30:04 +0530

    added target atribute in <a> tag to open a new tab

--- src/componenets/WorkCard.js
+++ src/componenets/WorkCard.js
@@ -11,9 +11,10 @@ const WorkCard = (props) => {
         <div className="pro-details">
             <p>{props.text}</p>
             <div className="pro-btns">
-                <NavLink to= {props.view} className= "btn" >View</NavLink>
-                <NavLink to={props.source} className= "btn" >Source</NavLink>
-            </div>
+    <a href={props.view} target="_blank" rel="noopener noreferrer" className="btn">View</a>
+    <a href={props.source} target="_blank" rel="noopener noreferrer" className="btn">Source</a>
+</div>
+
         </div>
         </div>
   )
